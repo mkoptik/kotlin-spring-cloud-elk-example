0e70552bbbf3cb29a5dd57b3d42ed620a038fe4c
Unknown <koptik.m@gmail.com>
2017-12-08 13:21:46 +0100

    Added typescript/react/redux frontend

---
+++ frontend/src/containers/App/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import * as style from './style.css';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { RootState } from '../../reducers';
+
+export namespace App {
+  export interface Props extends RouteComponentProps<void> {
+
+  }
+
+  export interface State {
+
+  }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+export class App extends React.Component<App.Props, App.State> {
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div className={style.normal}>Hello from App</div>
+    );
+  }
+}
+
+function mapStateToProps(state: RootState) {
+  return {
+
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+  };
+}
